69dc0131cd775ade510a9c0befd6b67a44fb9d12
UmbertoB <umbertobarrosf@gmail.com>
2019-02-07 10:48:53 -0300

    [master] | refactors

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -85,41 +85,51 @@ export class DashboardComponent implements OnInit {
             max_date: moment(this.endDate).format('YYYY-MM-DD HH:mm')
         };
 
-        this.sessionsService.get({ query: lastWeek, url: 'statistics' }).subscribe(
-            res => {
-                this.attendedPatients = res.totalPatients;
-                this.totalHoursWorked = res.totalHours;
+        // this.sessionsService.get({ query: lastWeek, url: 'statistics' })
+        //     .subscribe(
+        //         res => {
+        //             this.attendedPatients = res.totalPatients;
+        //             this.totalHoursWorked = res.totalHours;
 
-                if (this.lastWeekSessionsChart) this.lastWeekSessionsChart.destroy();
-                this.lastWeekSessionsChart = new Chart('lastWeekSessionsChart', {
-                    type: 'line',
-                    beginAtZero: true,
-                    data: {
-                        labels: res.dataLabels,
-                        datasets: [{
-                            data: res.dataSets,
-                            borderColor: 'white',
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        responsive: true,
-                        legend: {
-                            display: false,
-                        },
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true,
-                                    callback: function (value) { if (Number.isInteger(value) && value >= 0) { return value; } },
-                                    stepSize: 1
-                                }
+                    if (this.lastWeekSessionsChart) this.lastWeekSessionsChart.destroy();
+                    this.lastWeekSessionsChart = new Chart('lastWeekSessionsChart', {
+                        type: 'bar',
+                        data: {
+                            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                            datasets: [{
+                                label: '# of Votes',
+                                data: [12, 19, 3, 5, 2, 3],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255,99,132,1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
                             }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
                         }
-                    }
-                });
-            }
-        );
+                    });
+                // }
+            // );
     }
 
     public recalculateSessions() {
