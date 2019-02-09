816b5a24a2d7d53f9ca8c099187aa2727c4e7b5c
UmbertoB <umbertobarrosf@gmail.com>
2019-02-09 02:04:31 -0200

    [production] | 09/02/2019 - 02:04

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/dist/patients-dashboard'));
+app.use(express.static(__dirname + '/dist/salvus-dashboard'));
 app.listen(process.env.PORT || 3000);
